3cf7f563c4097aa2bd6dfd199727ed64895d354c
aybiya <yanez.aybi@gmail.com>
2024-09-06 09:29:31 -0300

    probando mi app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://aybiya.github.io/portfolio_aybi-yanez/',
+  base: '/portfolio_aybi-yanez/',
 })
